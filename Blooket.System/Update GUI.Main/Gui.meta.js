070fe8358e29ad05a76c4c7431d000a178161feb
Nuker214 <139010113+Nuker214@users.noreply.github.com>
2025-08-14 21:18:38 -0400

    UPDATE

---
+++ Blooket.System/Update GUI.Main/Gui.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Blooket Cheats Plus
+// @namespace    https://github.com/randomstuff69/blooketcheatsplus
+// @version      15
+// @description  adds cheats from blooketbot.glitch.me to the 05konz gui
+// @updateURL    https://raw.githubusercontent.com/randomstuff69/blooketcheatsplus/main/Update/Gui.meta.js
+// @downloadURL  https://raw.githubusercontent.com/randomstuff69/blooketcheatsplus/main/GUI/Gui.user.js
+// @author       DannyDan0167 and Cool Duck
+// @match        *://*.blooket.com/*
+// @icon         https://i.ibb.co/sWqBm0K/1024.png
+// @grant        none
+// @require     https://raw.githubusercontent.com/randomstuff69/blooketcheatsplus/refs/heads/main/GUI/Gui.js
+// @license MIT
+// ==/UserScript==
+
