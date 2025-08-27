692bbc737513d800ee4e04a805500d150be315a4
Nuker214 <139010113+Nuker214@users.noreply.github.com>
2025-08-26 21:58:10 -0400

    Update User.js

--- api/auth/logs/user/admin/models/User.js
+++ api/auth/logs/user/admin/models/User.js
@@ -1,1 +1,29 @@
+const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    userID: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        default: 'User'
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'], 
+        default: 'user'
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
