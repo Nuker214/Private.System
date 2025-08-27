9f11a81bc5c640a74a216f3bca888848b3caeb00
Nuker214 <139010113+Nuker214@users.noreply.github.com>
2025-08-26 21:58:46 -0400

    Update ActivityLog.js

--- api/auth/logs/user/admin/models/ActivityLog.js
+++ api/auth/logs/user/admin/models/ActivityLog.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const ActivityLogSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    username: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now },
+    activity: { type: String, required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.models.ActivityLog || mongoose.model('ActivityLog', ActivityLogSchema);
